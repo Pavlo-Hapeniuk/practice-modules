aa6a11dfbd4f9b850f19ef6d24c8cf6db0680d03
Pavlo-Hapeniuk <johnybfrank@gmail.com>
2026-05-20 16:07:20 +0000

    Deploying to gh-pages from @ Pavlo-Hapeniuk/practice-modules@430f4a50bdf25f7eb3caa6c413ae881608d376c8 🚀

---
+++ app.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DD3qoKza.js";const e="/practice-modules/assets/kitten-DJQp-i6p.avif";document.querySelector("#app").innerHTML=`<img src="${e}" width="400"/>`;console.log(e);
+//# sourceMappingURL=app.js.map
